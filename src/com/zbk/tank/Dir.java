858c334e47260aca4999264367efe29e8dd0945e
zbk <zbk0708@163.com>
2020-05-28 02:24:30 +0800

    add a enum file of Tank Direction and use this enum to control the direction of tank

---
+++ src/com/zbk/tank/Dir.java
@@ -0,0 +1,9 @@
+package com.zbk.tank;
+
+/**
+ * @author ZBK
+ * @date 2020/5/28 - 2:01
+ */
+public enum Dir {
+    UP,DOWN,LEFT,RIGHT,DEFAULT;
+}
--- src/com/zbk/tank/TankFrame.java
+++ src/com/zbk/tank/TankFrame.java
@@ -21,7 +21,8 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
 
     int x = 200, y=200;
-    MyKeyListener myKeyListener = new MyKeyListener();
+    Dir dir = Dir.DEFAULT;
+    private static final int SPEED = 10;
     public TankFrame() throws HeadlessException {
         setSize(800,600);
         setResizable(false);
@@ -36,21 +37,28 @@ public class TankFrame extends Frame {
         });
 
 
-        addKeyListener(myKeyListener);
+        addKeyListener(new MyKeyListener());
     }
 
     @Override
     public void paint(Graphics g) {
-        if (myKeyListener.bL)
-            x -= 10;
-        if (myKeyListener.bR)
-            x += 10;
-        if (myKeyListener.bU)
-            y -= 10;
-        if (myKeyListener.bD)
-            y += 10;
         g.fillRect(x, y, 50, 50);
-
+        switch (dir){
+            case UP:
+                y -= 10;
+                break;
+            case DOWN:
+                y += 10;
+                break;
+            case LEFT:
+                x -= 10;
+                break;
+            case RIGHT:
+                x += 10;
+                break;
+            default:
+                break;
+        }
     }
 
     class MyKeyListener extends KeyAdapter{
@@ -78,6 +86,7 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
+            setMainTankDir();
         }
 
         @Override
@@ -99,6 +108,16 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
+            setMainTankDir();
         }
+
+        private void setMainTankDir() {
+            if(bL) dir = Dir.LEFT;
+            else if(bR) dir = Dir.RIGHT;
+            else if(bU) dir = Dir.UP;
+            else if(bD) dir = Dir.DOWN;
+            else dir = Dir.DEFAULT;
+        }
+
     }
 }
